b62a2f2c6edd3ee726fe4b67602e027a6aa74760
Alex <aleksandrloginov03@ya.ru>
2024-12-25 12:53:40 +0300

    Inthial commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["timeOpen"])) {
+    $_SESSION["timeOpen"] = time();
+    echo "Добро пожаловать! Это ваш первый визит.";
+} else {
+    $res = time() - (int)$_SESSION['timeOpen'];
+    echo "Вы зашли на сайт $res секунд назад.";
+
+    //$_SESSION["timeOpen"] = time();
+}
+?>
+
+<a class="button" href="register.php">Кнопка</a>
